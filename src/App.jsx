af993164999c211138c8e62952e0d2bd56796e9f
Kieran Edwards <kieran.edwards@vml.com>
2025-05-31 10:24:35 +0100

    Split journeys

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ function App() {
 
   return (
     <>
-      <main className="flex flex-col lg:pt-16 pb-4">
-        <TimetableWrap stationCode1="CFH" destCode1="FST" stationCode2="EUS" destCode2="WFJ" title="inbound"/>
-        <TimetableWrap stationCode1="WFJ" destCode1="EUS" stationCode2="FST" destCode2="CFH" title="outbound"/>
+      <main className="flex lg:flex-row flex-col lg:pt-16 pb-4">
+        <TimetableWrap stationCode="CFH" destCode="FST" title="Chafford to Fenchurch"/>
+        <TimetableWrap stationCode2="FST" destCode2="CFH" title="Fenchurch to Chafford"/>
+        <TimetableWrap stationCode="EUS" destCode="WFJ" title="Euston to Watford"/>
+        <TimetableWrap stationCode1="WFJ" destCode1="EUS" title="Watford to Euston"/>
       </main>
     </>
   )
