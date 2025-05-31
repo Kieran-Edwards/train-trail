2314c7dc6a6377818aacfdfe2311bce7ebc6483d
Kieran-Edwards <117165363+Kieran-Edwards@users.noreply.github.com>
2025-05-31 11:38:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
     <>
       <main className="flex lg:flex-row flex-col lg:pt-16 pb-4">
         <TimetableWrap stationCode="CFH" destCode="FST" title="Chafford to Fenchurch"/>
-        <TimetableWrap stationCode2="FST" destCode2="CFH" title="Fenchurch to Chafford"/>
+        <TimetableWrap stationCode="FST" destCode="CFH" title="Fenchurch to Chafford"/>
         <TimetableWrap stationCode="EUS" destCode="WFJ" title="Euston to Watford"/>
-        <TimetableWrap stationCode1="WFJ" destCode1="EUS" title="Watford to Euston"/>
+        <TimetableWrap stationCode="WFJ" destCode="EUS" title="Watford to Euston"/>
       </main>
     </>
   )
