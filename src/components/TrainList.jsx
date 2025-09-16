6d48946a271458df65c12cdcb3a88bb7ad8449f4
Kieran Edwards <kieran.edwards@vml.com>
2025-09-16 15:26:18 +0100

    Add delay information

--- src/components/TrainList.jsx
+++ src/components/TrainList.jsx
@@ -10,100 +10,88 @@ export function TrainList({ trainData, destCode }) {
             try {
                 const formattedDate = GetFormattedDate();
 
-                const detailsMap = {};
+                const detailsMap = Object.fromEntries(
+                    await Promise.all(
+                        trainData.map(async ({ serviceUid }) => {
+                            const response = await fetch(
+                                `https://rtt.07edwardsk.workers.dev/proxy/service/${serviceUid}${formattedDate}`
+                            );
 
-                await Promise.all(
-                    trainData.map(async (trainService) => {
-                        const response = await fetch(
-                            `https://rtt.07edwardsk.workers.dev/proxy/service/${trainService.serviceUid}${formattedDate}`
-                        );
+                            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-                        if (!response.ok) {
-                            throw new Error(`HTTP error! status: ${response.status}`);
-                        }
+                            const data = await response.json();
+                            const currentLocation = data.locations.find(
+                                (loc) =>
+                                    loc.realtimeActivated &&
+                                    (!loc.realtimeDepartureActual || loc.realtimeArrivalActual)
+                            );
+                            const destLocation = data.locations.find((loc) => loc.crs === destCode);
 
-                        const data = await response.json();
-
-                        const currentLocation = data.locations.find(
-                            (location) =>
-                                location.realtimeActivated &&
-                                (!location.realtimeDepartureActual || location.realtimeArrivalActual)
-                        );
-
-                        const destLocation = data.locations.find((location) => location.crs === destCode);
-
-                        detailsMap[trainService.serviceUid] = {
-                            currentLocation: currentLocation || null,
-                            destLocation: destLocation || null,
-                        };
-                    })
+                            return [serviceUid, { currentLocation: currentLocation || null, destLocation: destLocation || null }];
+                        })
+                    )
                 );
 
                 setServiceDetailsMap(detailsMap);
-                setLoadingServices(false);
             } catch (err) {
                 console.error(err.message);
+            } finally {
                 setLoadingServices(false);
             }
         };
 
         fetchAllServiceDetails();
-    }, [trainData, destCode, setServiceDetailsMap]);
+    }, [trainData, destCode]);
+
+    if (loadingServices) return <p>Loading service details...</p>;
 
     return (
         <div className="flex flex-col mb-4">
-            {loadingServices ? (
-                <p>Loading service details...</p>
-            ) : (
-                trainData.map((trainService) => {
-                    const serviceDetails = serviceDetailsMap[trainService.serviceUid];
-                    return (
-                        <div key={trainService.serviceUid} className="flex text-black text-left p-4 border-b border-slate-400">
-                            <p className="mr-3">
-                                {FormatTime(
-                                    trainService.locationDetail.gbttBookedArrival
-                                        ? trainService.locationDetail.gbttBookedArrival
-                                        : trainService.locationDetail.gbttBookedDeparture
-                                )}
+            {trainData.map((trainService) => {
+                const { locationDetail, serviceUid } = trainService;
+                const serviceDetails = serviceDetailsMap[serviceUid];
+
+                const departureTime = locationDetail.gbttBookedArrival || locationDetail.gbttBookedDeparture;
+                const timeDifference = locationDetail.realtimeDeparture - departureTime;
+
+                return (
+                    <div key={serviceUid} className="flex text-black text-left p-4 border-b border-slate-400">
+                        <p className={`mr-3 ${locationDetail.realtimeDepartureActual ? 'text-red-500' : ''}`}>{FormatTime(departureTime)}</p>
+
+                        <p className="mr-3">to</p>
+
+                        <div className="flex-grow">
+                            <p>{locationDetail.destination[0].description}</p>
+
+                            <p className="text-xs">
+                                {locationDetail.realtimeDepartureActual
+                                    ? `Departed: ${FormatTime(locationDetail.realtimeDeparture)}`
+                                    : timeDifference > 0
+                                    ? `Expected: ${FormatTime(locationDetail.realtimeDeparture)}`
+                                    : null}
+                                {timeDifference !== 0 &&
+                                    ` (${timeDifference > 0 ? "+" : "-"}${Math.abs(timeDifference)}min)`}
                             </p>
-                            <p className="mr-3">to</p>
-                            <div className="flex-grow">
-                                <p>{trainService.locationDetail.destination[0].description}</p>
-                                <p className="text-xs">
-                                    <span>Expected: </span>
-                                    <span>
-                                        {FormatTime(
-                                            trainService.locationDetail.gbttBookedArrival
-                                                ? trainService.locationDetail.realtimeArrival
-                                                : trainService.locationDetail.realtimeDeparture
-                                        )}
-                                    </span>
-                                </p>
-                                <p className="text-xs">
-                                    <span>Arrives at {destCode}: </span>
-                                    {serviceDetails && (
-                                        <span>
-                                            {serviceDetails.destLocation.realtimeArrival
-                                                ? FormatTime(serviceDetails.destLocation.realtimeArrival)
-                                                : "N/A"}
-                                        </span>
-                                    )}
-                                </p>
-                            </div>
-                            <div className="text-center">
-                                <p>Plat</p>
-                                <p
-                                    className={
-                                        trainService.locationDetail.platformConfirmed ? "text-green-500" : "text-red-500"
-                                    }
-                                >
-                                    {trainService.locationDetail.platform}
-                                </p>
-                            </div>
+
+                            {locationDetail.cancelReasonShortText}
+
+                            <p className="text-xs">
+                                <span>Arrives at {destCode}: </span>
+                                {serviceDetails?.destLocation?.realtimeArrival
+                                    ? FormatTime(serviceDetails.destLocation.realtimeArrival)
+                                    : "N/A"}
+                            </p>
                         </div>
-                    );
-                })
-            )}
+
+                        <div className="text-center">
+                            <p>Plat</p>
+                            <p className={locationDetail.platformConfirmed ? "text-green-500" : "text-red-500"}>
+                                {locationDetail.platform}
+                            </p>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 }
